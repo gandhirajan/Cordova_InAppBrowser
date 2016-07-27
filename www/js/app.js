a1b76024deba5f40c4e6383704072eedec34c408
gandhirajan <gandhirajan.n@gmail.com>
2016-07-27 16:08:29 +0530

    Exit on back button press from InAppBrowser page

--- www/js/app.js
+++ www/js/app.js
@@ -2,15 +2,24 @@ $(document).ready(function() {
 	document.addEventListener("deviceready", onDeviceReady, false);
 });
 
+var ref;
+
 function onDeviceReady() {		
      $('#inapp').click( function() 
 		{	
+			
 		    try {				
-				var ref = cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=yes');
+				ref = cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=yes');
+				ref.addEventListener("exit", onBackButton, false); 
 			}
 			catch(err) {
 				alert("Plugin Error - " + err.message);
 			}
 			
 		});		
+
+		function onBackButton(e) {
+			alert("back button pressed");
+			navigator.app.exitApp();
+		}
 }
